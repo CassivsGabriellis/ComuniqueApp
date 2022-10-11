7d20e52da8c12880c8ed9fe731df8c65a23ca6c4
CassivsGabriellis <cassiogabrielcontato@gmail.com>
2022-10-10 22:21:50 -0300

    Ideias iniciais do chat

--- Views/Chat.js
+++ Views/Chat.js
@@ -4,9 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 
 export default class telaChat extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            mensagens: [],
+            texto: ''
+        }
+    }
+
     render() {
         <Text>
-            
+
         </Text>
     }
 }
