2043357853bc91a8a30037ddf30b017d4bec7d70
CassivsGabriellis <cassiogabrielcontato@gmail.com>
2022-10-10 22:08:02 -0300

    Adicionando, inicialmente, a tela do chat

---
+++ Views/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+
+export default class telaChat extends React.Component {
+    render() {
+        <Text>
+            
+        </Text>
+    }
+}
